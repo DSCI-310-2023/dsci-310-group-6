df34203d8c70180801d81130a109bb1f866e2043
Davis Li <davismxli@gmail.com>
2023-03-11 15:35:30 -0800

    added tests for pairplot function

--- src/create_pairplot.R
+++ src/create_pairplot.R
@@ -1,3 +1,13 @@
+# Author: Davis Li
+# Date: 2023-03-11
+
+"This script takes a dataset and generates a pairplot plotting a scatter chart 
+and showing correlation between attributes. Also allows a title to be placed
+
+Usage: create_pairplot.R
+"
+
+
 library(ggplot2)
 library(GGally)
 
