1e565ab30322e487d9024295a75e7a312f5777b4
JialuJin <xyxxjinjialu@163.com>
2023-03-11 00:23:22 -0800

    edited Dockerfile, deleted useless commands for functions

--- tests/testthat/summary_data_test.R
+++ tests/testthat/summary_data_test.R
@@ -6,8 +6,15 @@ my_data <- data.frame(x = c(1, 2, 3, 4, 5),
                       y = c(10, 20, 30, 40, 50),
                       z = c(100, 200, 300, 400, 500))
 
-# Get the maximum value of the 'y' column
-expect_equal(get_max(my_data, "y"), 50)
-expect_equal(get_min(my_data, "z"), 100)
-expect_equal(get_med(my_data, "x"), 3)
+test_that("`summary_data` should return 50", {
+  expect_equal(get_max(my_data, "y"), 50)
+})
+
+test_that("`summary_data` should return 100", {
+  expect_equal(get_min(my_data, "z"), 100)
+})
+
+test_that("`summary_data` should return 100", {
+  expect_equal(get_med(my_data, "x"), 3)
+})
 
